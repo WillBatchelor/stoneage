1138a366b151d5d969cf2c5a75f67b823d398769
WillBatchelor <batchelorwd8@gmail.com>
2016-04-23 23:14:52 -0400

    Added arrow, flint and steel, sap, resin, cotton sheet, and fletch craftitems to init.lua

--- init.lua
+++ init.lua
@@ -8,6 +8,47 @@ minetest.register_craftitem("stoneage:flint", {
 })
 
 
+minetest.register_craftitem("stoneage:cotton_sheet", {
+	description = "Cotton Sheet",
+	inventory_image = "stoneage_cotton_sheet.png",
+
+})
+
+minetest.register_craftitem("stoneage:fletch", {
+    description = "Fletch",
+    inventory_image = "stoneage_fletch.png",
+
+})
+
+
+minetest.register_craftitem("stoneage:sap", {
+    description = "Sap",
+    inventory_image = "stoneage_sap.png",
+
+})
+
+
+minetest.register_craftitem("stoneage:fletch", {
+    description = "Resin",
+    inventory_image = "stoneage_resin.png",
+
+})
+
+
+minetest.register_craftitem("stoneage:arrow", {
+    description = "Arrow",
+    inventory_image = "stoneage_arrow.png",
+
+})
+
+
+minetest.register_craftitem("stoneage:flint_and_steel", {
+    description = "Flint and Steel",
+    inventory_image = "stoneage_flint_and_steel.png",
+
+})
+
+
 minetest.override_item("default:gravel", {drop = {
 	max_items = 1,
 	items = {
